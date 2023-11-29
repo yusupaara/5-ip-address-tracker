d4e7160bb90e20fb5553dbc3737aafa2e898e115
yusupaara <yoesoep17@gmail.com>
2023-11-29 12:16:36 +0700

    Revert "first commit"
    This reverts commit fc48ae9a1ec05186f62c283fbccc887003f8da11.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import SimpleMap from './components/renderMap';
 import DetectLocation from './components/detectLocation';
-import SavedHistory from './components/savedHistory';
 
 function App() {
   const [mapLocation, setMapLocation] = useState(null);
 
   return (
     <div>
-      <SavedHistory />
       <DetectLocation setMapLocation={setMapLocation} />
       <SimpleMap mapLocation={mapLocation} />
     </div>
